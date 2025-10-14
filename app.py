e2feebe1d60b9f66d5d7e31fb14a3488feba3ad9
narutousomaki741 <narutousomaki741@gmail.com>
2025-10-14 19:41:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import logging
 import yaml
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 # Try to load config, use defaults if not available
